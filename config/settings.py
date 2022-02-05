db51a2831680a50d233ea0865db30833086563f8
rlb@x24 <baxrob@gmail.com>
2022-02-04 17:40:19 -0800

    ...

--- config/settings.py
+++ config/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES = {
-    'default': env.db_url()
-}
+#DATABASES = {
+#    'default': env.db_url()
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
