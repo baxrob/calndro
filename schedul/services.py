17267f96a893d5b7527d3bda38ed84a92d4bbcdb
rlb@x23 <baxrob@gmail.com>
2022-02-15 19:01:46 -0800

    ...

---
+++ schedul/services.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from django.utils import timezone
+from django.contrib.auth import login
+from django.core.mail import send_mail
+
+from rest_framework.exceptions import ValidationError
+
+from schedul.models import EmailToken
+from schedul.serializers import DispatchLogEntrySerializer
+
+
+def notify(event, sender_email, recip_email):
+    recipient = User.objects.get(email=recip_email)
+    token = EmailToken.objects.create(event=event, user=recipient)
+    url_base = event.get_absolute_url()
+    url = '%s?et=%s' % (url_base, token.key)
+    msg = 'expires %s' % token.expires
+    message = '%s\n%s' %(url, msg)
+    subject = 'Event %d Updated' % evt.id
+    from_email = sender_email
+    recipient_list = [recip_email]
+    print(message)
+    return
+    send_mail(subject, message, from_email, recipient_list) 
+
+def token_login(request, event):
+    key = request.GET.get('et')
+    try:
+        et = EmailToken.objects.get(pk=key)
+        now = timezone.now()
+        # X: custom err on expired ?
+        if request.user != et.user and et.event == event:# and now < et.expires:
+            if now >= et.expires:
+                raise ValidationError(['Token Expired'])
+            login(request, et.user) 
+            return et
+        else:
+            return None
+    except EmailToken.DoesNotExist:
+        return None
+    
+
+def enlog(event, effector, occurrence, slots, data={}):
+    serializer = DispatchLogEntrySerializer(data={'event': event,
+        'effector': effector, 'occurrence': occurrence, 'slots': slots,
+        'data': data})
+    if serializer.is_valid():
+        entry = serializer.save()
+        return serializer.data
+    else:
+        return serializer.errors
