5f7f33eaa89fd0eae6636942efe9bb0a0aab1588
rlb@x23 <baxrob@gmail.com>
2023-05-12 18:27:46 -0700

    add passenger wsgi

---
+++ config/dh_wsgi.py
@@ -0,0 +1,17 @@
+import sys, os
+INTERP = "/home/bhand/cald.blandhand.net/venv/bin/python3"
+#INTERP is present twice so that the new python interpreter 
+#knows the actual executable path 
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/calndro')  #You must add your project here
+
+sys.path.insert(0,cwd+'/venv/bin')
+sys.path.insert(0,cwd+'/venv/lib/python3.8/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "config.settings"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
