f0f400898c2c8de516ce618802fffe6458baae81
ob@t48 <rlb@blandhand.net>
2023-05-18 10:46:50 -0700

    add_users fix

--- scripts/add_users.py
+++ scripts/add_users.py
@@ -6,8 +6,9 @@ from getpass import getpass
 import django
 from django.contrib.auth import get_user_model
 
-cwd = os.getcwd()
-sys.path.append(cwd + '/..')
+#import ipdb; ipdb.set_trace()
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(scripdir + '/..')
 os.environ['DJANGO_SETTINGS_MODULE'] = "config.settings"
 django.setup()
 
@@ -19,7 +20,7 @@ while True:
     password = getpass()
     perms = input('Enter permissions [active, staff, super] (100): ')
     perms = '100' if len(perms) == 0 else perms
-    do_create = input('Confirm and create %s %s %s Y/n:'
+    do_create = input('Confirm and create (%s %s %s) [Y/n]:'
         % (name, email, perms))
     if do_create in ['n', 'N']:
         continue
@@ -29,8 +30,9 @@ while True:
     user.is_staff = True if perms[1] == '1' else False
     user.is_superuser = True if perms[2] == '1' else False
     user.save()
-    print(user)
+    print(user.username, user.email, user.is_active, user.is_staff,
+        user.is_superuser)
 
-    contin = input('Continue Y/n: ')
+    contin = input('Continue [Y/n]: ')
     if contin in ['n', 'N']:
         break
