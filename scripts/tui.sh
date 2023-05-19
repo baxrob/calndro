9c9be531d41ebe4a64deff9d335b2a8cf2040622
rlb@x23 <baxrob@gmail.com>
2023-05-19 11:29:37 -0700

    tui fixes

--- scripts/tui.sh
+++ scripts/tui.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 
-DEFAULT_USER=ob
-DEFAULT_PASS=p
-DEFAULT_SENDER=ob@localhost
-
-user=${user:-$DEFAULT_USER}
-pass=${pass:-$DEFAULT_PASS}
 host=${host:-}
-port=${port:-8000}
-sender=${sender:-$DEFAULT_SENDER}
+port=${port:-} 
+#sender=${sender:-}
+print=${print:-b}
 
-echo config: ${user:-_} ${pass:-_} ${host:-_} ${port:-_} $sender
+user=${user:-}
+pass=${pass-} # declare as empty to prompt
+session=${session:-}
+token=${token:-}
+
+auth=
+if [ -n "$token" ]; then
+    auth="-a $token"
+elif [ -n "$user" ]; then
+    while [ -z "$pass" ]; do
+        echo -n 'Password: '
+        pass=$(bash -c 'read -s x; echo $x')
+        echo
+        [ -z "$pass" ] && echo got blank
+    done
+    auth="-a $user:$pass"
+fi
+if [ -n "$session" ]; then
+    auth="$auth --session=$session"
+fi
+
+showauth=$(echo $auth | sed 's/:[^ ]*/:***/')
+printf "config:\n  auth: $showauth\n  addr: $host:$port\n  print: $print\n"
 
 which jq > /dev/null || { echo jq required; exit 1; }
 
@@ -32,49 +49,66 @@ prompt() {
     printf "patch: enter slots by number> "
     ;;
     
-    notify)
+    notify_*)
+    echo prompt notify ${1#*_}
     p=$(req detail $2 | jq .parties)
     nlist "$p"
-    printf "notify: enter parties by number> "
-    ;;
-    notify_parties)
-    ;;
-    notify_sender)
+    if [ ${1#*_} = parties ]; then
+        printf "notify: enter parties by number> "
+    elif [ ${1#*_} = sender ]; then
+        printf "enter sender by number or blank for [1]> "
+    fi
     ;;
     
     *) printf "  [nop $1 $2\n]" ;;
     esac
 }
 
-# X:
-sess() {
-    :
-}
-
 req() {
+    #echo req $@
     case $1 in
-    list) http --print=b -a $user:$pass $host:$port/ ;;
-    detail) http --print=b -a $user:$pass $host:$port/$2/ ;;
-    log) http --print=b -a $user:$pass $host:$port/$2/log/ ;;
+    #list) http --print=b -a $user:$pass $host:$port/ ;;
+    #detail) http --print=b -a $user:$pass $host:$port/$2/ ;;
+    #log) http --print=b -a $user:$pass $host:$port/$2/log/ ;;
+    list) http --print=$print $auth $host:$port/ ;;
+    detail) http --print=$print $auth $host:$port/$2/ ;;
+    log) http --print=$print $auth $host:$port/$2/log/ ;;
     create)
-    http --print=b -a $user:$pass $host:$port/ parties:="$2" slots:="$3" \
-        title="$4"
+    #titledecl=$([ -n "$4" ] && printf title="$4" || printf "")
+    #echo $titledecl
+    hcmd="http --print=$print $auth $host:$port/ parties:="$2" slots:="$3""
+    #hcmd="http --print=b -a $user:$pass $host:$port/ parties:="$2" slots:="$3""
+    if [ -n "$4" ]; then
+        hcmd="$hcmd title="$4""
+    fi
+    echo $($hcmd)
+    #http --print=b -a $user:$pass $host:$port/ parties:="$2" \
+    #    slots:="$3" "$titledecl"
+       # title="$4"
     ;;
-    patch) http --print=b -a $user:$pass PATCH $host:$port/$2/ slots:="$3" ;;
+    #patch) http --print=b -a $user:$pass PATCH $host:$port/$2/ slots:="$3" ;;
+    patch) http --print=$print $auth PATCH $host:$port/$2/ slots:="$3" ;;
     notify)
-    http --print=b -a $user:$pass POST $host:$port/$2/notify/ \
-        parties:="$3" slots:="$4"
+    #echo $@
+    #echo http --print=b -a $user:$pass POST $host:$port/$2/notify/ \
+    #    parties:="$3" slots:="$4" sender=$5
+    #http --print=b -a $user:$pass POST $host:$port/$2/notify/ \
+    #    parties:="$3" slots:="$4" sender=$5
+    echo http --print=$print $auth POST $host:$port/$2/notify/ \
+        parties:="$3" slots:="$4" sender=$5
+    http --print=$print $auth POST $host:$port/$2/notify/ \
+        parties:="$3" slots:="$4" sender=$5
     ;;
     esac
 }
 
 create() {
-    plist=[
+    plist='[' # quoted for vim syntax highlighting
     for eml in $1; do
         plist="$plist\"$eml\","
     done
     plist=$(echo $plist | sed 's/,*$/]/')
-    slist=[
+    slist='['
     slist=$slist$(echo $2 | awk '/./ {
         printf("{\"begin\": \"%s\", \"duration\": \"%s\"},", $1, $2)
     }')
@@ -90,7 +124,7 @@ nlist() {
 }
 
 listn() {
-    list=[
+    list='['
     for i in $1; do
         list="$list$(($i - 1)),"
     done
@@ -102,6 +136,8 @@ evtnum=
 cparties=
 cslots=
 
+nparties=
+#nsender=
 
 
 ## Run loop ##
@@ -109,7 +145,6 @@ cslots=
 pcode=menu
 
 prompt $pcode
-
 
 # X: posix but no readline 
 #while read cmd; do
@@ -121,8 +156,8 @@ while cmd=$(bash -c 'read -er cmd; echo $cmd'); do
         c) pcode=create_title ;;
         d[0-9]*) req detail ${cmd#d} ;;
         p[0-9]*) evtnum=${cmd#p}; pcode=patch ;;
-        n[0-9]*) evtnum=${cmd#n}; pcode=notify ;;
-        #n[0-9]*) evtnum=${cmd#n}; pcode=notify_sender ;;
+        #n[0-9]*) evtnum=${cmd#n}; pcode=notify ;;
+        n[0-9]*) evtnum=${cmd#n}; pcode=notify_parties ;;
         g[0-9]*) req log ${cmd#g} ;;
         '?')
         printf "l list events\n"
@@ -148,23 +183,37 @@ while cmd=$(bash -c 'read -er cmd; echo $cmd'); do
         pcode=create_slots_nop
     else
         create "$cparties" "$cslots" "$ctitle"
+        cslots=
         pcode=menu
     fi
     ;;
     patch)
     #listn "$cmd" "$(req detail $evtnum | jq .slots)"
-    s=$(listn "$cmd" "$(req detail $evtnum | jq .slots)")
+    s=$(listn "$cmd" "$(req detail $evtnum | jq -c .slots)")
     #printf "ps $s\n"
     #s=$cmd
     req patch $evtnum "$s"
     pcode=menu
     ;;
     notify)
-    p=$(listn "$cmd" "$(req detail $evtnum | jq .parties)")
-    #p="$cmd"
-    s=$(req detail $evtnum | jq .slots)
-    req notify $evtnum "$p" "$s"
+    parties=$(listn "$cmd" "$(req detail $evtnum | jq -c .parties)")
+    slots=$(req detail $evtnum | jq -c .slots)
+    req notify $evtnum "$parties" "$slots"
     pcode=menu
+    ;;
+    notify_*)
+    if [ ${pcode#*_} = parties ]; then
+        nparties=$(listn "$cmd" "$(req detail $evtnum | jq -c .parties)")
+        pcode=notify_sender
+    elif [ ${pcode#*_} = sender ]; then
+        cmd=$([ -z "$cmd" ] && echo 1 || echo "$cmd")
+        sender=$(listn "$cmd" "$(req detail $evtnum | jq -c .parties)" \
+            | jq .[0] | sed 's/"//g')
+        echo Sender $sender
+        slots=$(req detail $evtnum | jq .slots)
+        req notify $evtnum "$nparties" "$slots" $sender
+        pcode=menu
+    fi
     ;;
 
     *) echo [whot? $cmd] ;;
